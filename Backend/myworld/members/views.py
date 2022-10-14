739174e0921affa5cc3aff0a8772fadc0daa9794
Samuel Darkow <97488839+sddark@users.noreply.github.com>
2022-10-13 22:09:14 -0400

    Add members

--- Backend/myworld/members/views.py
+++ Backend/myworld/members/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Members
 
 def index(request):
-    return HttpResponse("Hello world!")
+  mymembers = Members.objects.all().values()
+  output = ""
+  for x in mymembers:
+    output += x["firstname"]
+  return HttpResponse(output)
