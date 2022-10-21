3bb0c53749aefb90ddcbbfb7f436579e95675654
Samuel Darkow <97488839+sddark@users.noreply.github.com>
2022-10-20 22:13:03 -0400

    Added ability to delete record

--- Backend/myworld/myworld/urls.py
+++ Backend/myworld/myworld/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('members/', include('members.urls')),
+    path('', RedirectView.as_view(url='members/')),
     path('admin/', admin.site.urls),
 ]
